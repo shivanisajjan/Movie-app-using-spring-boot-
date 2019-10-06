2688d35ce920f023ad5c9374455925b19b6cd7a6
shivanisajjan <shivanisajjan269@gmail.com>
2019-10-06 12:55:42 +0530

    first commit

--- src/main/java/com/stackroute/userservice/seeddata/AppListenser.java
+++ src/main/java/com/stackroute/userservice/seeddata/AppListenser.java
@@ -3,12 +3,21 @@ package com.stackroute.userservice.seeddata;
 import com.stackroute.userservice.model.Movie;
 import com.stackroute.userservice.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppListenser implements ApplicationListener<ContextRefreshedEvent> {
+    @Value("${movie.id}")
+    private int id;
+    @Value("${movie.title}")
+    private String title;
+    @Value("${movie.original_language}")
+    private String original_lan;
+    @Value("${movie.overview}")
+    private String overview;
     @Autowired
     private MovieRepository movieRepository;
     public MovieRepository getMovieRepository() {
@@ -23,6 +32,6 @@ public class AppListenser implements ApplicationListener<ContextRefreshedEvent> 
         preFillData();
     }
     public void preFillData(){
-        this.movieRepository.save(new Movie(1,"happy days","B.tech life","telugu"));
+        this.movieRepository.save(new Movie(id,title,overview,original_lan));
     }
 }
--- src/main/java/com/stackroute/userservice/seeddata/CmdRunner.java
+++ src/main/java/com/stackroute/userservice/seeddata/CmdRunner.java
@@ -3,13 +3,24 @@ package com.stackroute.userservice.seeddata;
 import com.stackroute.userservice.model.Movie;
 import com.stackroute.userservice.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource(value = "classpath:cmdRunner.properties", ignoreResourceNotFound=true)
 public class CmdRunner implements CommandLineRunner {
+    @Value("${movie1.id}")
+    private int id;
+    @Value("${movie1.title}")
+    private String title;
+    @Value("${movie1.original_language}")
+    private String original_lan;
+    @Value("${movie1.overview}")
+    private String overview;
     @Autowired
     private MovieRepository movieRepository;
     public MovieRepository getMovieRepository() {
@@ -21,7 +32,7 @@ public class CmdRunner implements CommandLineRunner {
     }
 
     public void preFillData(){
-        this.movieRepository.save(new Movie(2,"happy","Family Entertainer","telugu"));
+        this.movieRepository.save(new Movie(id,title,overview,original_lan));
     }
 
     @Override
